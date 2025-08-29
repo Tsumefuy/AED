e9b81ecee86f75dbfe6d6dadda2f4dc2a03af99e
Gustavo <maiagustavo125@gmail.com>
2025-08-29 11:38:35 -0300

    update 2: delete, height, equal

--- pilha.h
+++ pilha.h
@@ -48,11 +48,67 @@ int top(tp_pilha *p, tp_item*e) {
 
 void print_pilha(tp_pilha p) {
 	tp_item e;
-	printf("\n");
+	printf("\nElements: ");
 	while (!empty_pilha(&p)) {
 		pop(&p, &e);
 		printf("%d ", e);
 	}
 	printf("\n");
 }
+
+void delete_impar(tp_pilha *p) {
+    tp_pilha pilha_par;
+    tp_item e;
+
+    start_pilha(&pilha_par);
+
+    while (!empty_pilha(p)) {
+        pop(p, &e);
+
+        if (e % 2 == 0)
+            push(&pilha_par, e);
+    }
+
+    while(!empty_pilha(&pilha_par)) {
+        pop(&pilha_par, &e);
+        push(p, e);
+    }
+}
+
+int height(tp_pilha *p) {
+    return p->top+1;
+}
+
+int equal(tp_pilha p1, tp_pilha p2) {
+    tp_item e1, e2;
+
+    if (height(&p1) != height(&p2))
+        return 0;
+
+    while(!empty_pilha(&p1)) {
+        pop(&p1, &e1);
+        pop(&p2, &e2);
+
+        if (e1 != e2) 
+            return 0;
+    }
+
+    return 1;
+}
+
+void concat(tp_pilha *p1, tp_pilha *p2) {
+    tp_item e;
+    tp_pilha paux;
+
+    while(!empty_pilha(p2)) {
+        pop(p2, &e);
+        push(&paux, e);
+    }
+
+    while(!empty_pilha(&paux)) {
+        pop(&paux, &e);
+        push(p2, e);
+    }
+}
+
 #endif
