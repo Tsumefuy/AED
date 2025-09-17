84d8f0c860246773cd40986e731545c66794f77c
Gustavo <maiagustavo125@gmail.com>
2025-09-17 08:51:37 -0300

    fix: corrigir implementação do quickselect e ajustar a função partition

--- algoritimos/quick_select.c
+++ algoritimos/quick_select.c
@@ -1,41 +1,54 @@
 #include <stdio.h>
 
-// Retorna o maior número (corrigir depois, está bugado)
+#define TAM 5
 
-int quickselect(int a[10], const int low, const int high, int k) {
+// Retorna o k-esimo maior número 
 
-    int i = low;
-    const int pivot = a[high-1];
+int partition(int a[TAM], const int l, const int h) {
+    int i = l;
+    const int pivot = a[h];
 
     int maior_que_pivot, menor_que_pivot;
 
-    for (int j = low; j < high; j++) { // 13 3 6 12
-        if (a[j] <= pivot) {
-            menor_que_pivot = a[j];
-            maior_que_pivot = a[i];
+    for (int j = l; j < h; j++) {
+        if (a[j] >= pivot) {
+            maior_que_pivot = a[j];
+            menor_que_pivot = a[i];
 
-            a[i] = menor_que_pivot;
-            a[j] = maior_que_pivot;
+            a[i] = maior_que_pivot;
+            a[j] = menor_que_pivot;
 
             i++;
         }
     }
 
-    maior_que_pivot = a[i]; // 3 6 13 12
-
+    menor_que_pivot = a[i]; 
     a[i] = pivot;
-    a[high] = maior_que_pivot; // 3 6 12 13
+    a[h] = menor_que_pivot; 
 
-    if (i < 9) return quickselect(a, i + 1, high, k);
-    else return a[k+9];
+    return i;
 }
+
+
+int quickselect(int a[TAM], const int low, const int high, int k) { 
+
+    int p_index = partition(a, low, high);
+
+    if (p_index > k) return quickselect(a, low, p_index-1, k);
+    else if (p_index < k) return quickselect(a, p_index+1, high, k);
+    else return a[k];
+}
+
 int main() {
-    int numbers[10];
+    int numbers[TAM], k;
 
     printf("Digite 10 numeros:");
-    for (int i = 0; i < 10; i++) scanf("%d", &numbers[i]);
+    for (int i = 0; i < TAM; i++) scanf(" %d", &numbers[i]);
 
-    int maior_numero = quickselect(numbers, 0, 10, 0);
-    printf("%d", maior_numero);
+    printf("Digite o k: ");
+    scanf(" %d", &k);
+
+    int k_esimo_numero = quickselect(numbers, 0, TAM-1, k-1);
+    printf("%d", k_esimo_numero);
     return 0;
 }
